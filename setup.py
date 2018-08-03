df9ed5431ca359773fde1ac29211e0bbe23c6dfa
Michael Howitz <mh@gocept.com>
2018-08-03 11:14:01 +0200

    Back to development: 4.2
    [skip ci]

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if not hasattr(shutil, 'get_terminal_size'):
 
 setup(
     name='toll',
-    version='4.1',
+    version='4.2.dev0',
     install_requires=install_requires,
     extras_require={
         'test': test_require,
